6aea8955c6766c344cfa29e7e439f80f077501c8
SunJi <wangsz86@163.com>
2025-03-24 20:48:09 +0800

    fix: 修改btn定位

--- src/components/LeftView/index.vue
+++ src/components/LeftView/index.vue
@@ -16,13 +16,16 @@
       </el-button>
     </el-upload>
 
-    <el-affix :offset="560">
-      <el-button type="primary">{{ $t("uploadCountLimit") }}</el-button>
-    </el-affix>
+    <div ref="affixButton">
+      <el-affix :offset="affixOffset">
+        <el-button type="primary">{{ $t("uploadCountLimit") }}</el-button>
+      </el-affix>
+    </div>
   </aside>
 </template>
 
 <script setup lang="ts">
+import { ref, onMounted, onUnmounted, nextTick } from 'vue'
 import { ElMessage } from 'element-plus';
 import { storeToRefs } from 'pinia'
 import { useCanvasImg } from '@/stores/canvasImg.ts';
@@ -37,15 +40,29 @@ interface UploadFile {
   raw: File
 }
 
+const affixOffset = ref(0)
+const affixButton = ref<HTMLElement>() // 获取按钮元素的引用
+
 const store = useCanvasImg()
 const { canvasFile, imgSrc } = storeToRefs(store)
 
 const maxFileSize = 5 // MB
 const errMsgSize = t('uploadMaxLimit', {maxFileSize: maxFileSize});
 
+// 计算偏移量
+const calculateOffset = () => {
+  nextTick(() => { // 确保 DOM 更新完成
+    console.log('affixButton.value', affixButton.value)
+    if (affixButton.value) {
+      // 使用 getBoundingClientRect 获取精确高度
+      console.log('affixButton.value', affixButton.value)
+      const buttonHeight = affixButton.value.getBoundingClientRect().height
+      affixOffset.value = window.innerHeight - buttonHeight - 20
+    }
+  })
+}
 // 文件验证
 const validateFile = (file: any) => {
-  console.log('file', file, file.type)
   const maxSize = maxFileSize * 1024 * 1024
   const isValidType = ['image/jpeg', 'image/png', 'image/jpg'].includes(file.raw.type)
 
@@ -76,6 +93,17 @@ const handleUpload = (file: UploadFile) => {
   if (!validateFile(file)) return
   loadImage(file.raw)
 }
+
+// 生命周期钩子
+onMounted(() => {
+  // 添加延迟确保元素渲染完成
+  calculateOffset()
+  window.addEventListener('resize', calculateOffset)
+})
+
+onUnmounted(() => {
+  window.removeEventListener('resize', calculateOffset)
+})
 </script>
 
 <style lang="less" scoped>
